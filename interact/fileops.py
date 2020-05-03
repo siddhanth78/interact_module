a6a29dc553320fcecb3bbf2f6d0d1cd9ad174c5e
Xstinkshun <61350672+siddhanth78@users.noreply.github.com>
2020-05-03 09:47:07 +0530

    Updated interact

---
+++ interact/fileops.py
@@ -0,0 +1,152 @@
+try:
+    import sys
+except:
+    print("os-sys (installation reqd. - yes")
+    time.sleep(3)
+    quit()
+else:
+    pass
+
+try:
+    import os
+except:
+    print("os (installation reqd. - yes")
+    time.sleep(3)
+    quit()
+else:
+    import os.path
+
+try:
+    import shutil
+except:
+    print("shutil (installation reqd. - yes")
+    time.sleep(3)
+    quit()
+else:
+    pass
+
+
+
+def readfile(filepath="",return_="whole",amt=0):
+    try:
+        file = open(filepath,'r')
+        file.close()
+    except:
+        print("\nError. Either file doesn't exist or an error occured.\n")
+        return
+    else:
+        file = open(filepath,'r')
+        if return_ == "whole":
+            if amt==0:
+                data=file.read()
+                file.close()
+                return data
+            else:
+                data=file.read(amt)
+                file.close()
+                return data
+        elif return_ == "lines":
+            if amt==0:
+                data=file.readlines()
+                file.close()
+                return data
+            else:
+                data=[]
+                for x in range(amt):
+                    data.append(file.readline())
+                file.close()
+                return data
+        else:
+            data=file.read()
+            file.close()
+            return data
+
+
+def writefile(filepath="",newdata="",mode="append"):
+    try:
+        file = open(filepath,'r')
+        file.close()
+    except:
+        print("\nError. Either file doesn't exist or an error occured.\n")
+        return
+    else:
+        if mode == "append":
+            file = open(filepath,'a')
+            file.write(newdata)
+            file.close()
+        elif mode == "overwrite":
+            file = open(filepath,'w')
+            file.write(newdata)
+            file.close()
+        else:
+            file = open(filepath,'a')
+            file.write(newdata)
+            file.close()
+
+
+def createfile(filepath=""):
+    if os.path.exists(filepath)==True:
+        print("\nFile already exists.\n")
+    elif os.path.exists(filepath)==False:
+        file = open(filepath,'x')
+        file.close()
+
+
+
+def deletefile(filepath=""):
+    try:
+        file = open(filepath,'r')
+        file.close()
+    except:
+        print("\nError. Either file doesn't exist or an error occured.\n")
+        return
+    else:
+        os.remove(filepath)
+
+
+
+def runfile(filepath=""):
+    try:
+        file = open(filepath,'r')
+        file.close()
+    except:
+        print("\nError. Either file doesn't exist or an error occured.\n")
+        return
+    else:
+        os.system(filepath)
+
+
+
+def rename(filepath="",newpath=""):
+    try:
+        file = open(filepath,'r')
+        file.close()
+    except:
+        print("\nError. Either file doesn't exist or an error occured.\n")
+        return
+    else:
+        os.rename(filepath,newpath)
+
+
+
+def movefile(filepath="",newpath=""):
+    try:
+        file = open(filepath,'r')
+        file.close()
+    except:
+        print("\nError. Either file doesn't exist or an error occured.\n")
+        return
+    else:
+        os.replace(filepath,newpath)
+
+
+
+def copyfile(filepath="",copypath=""):
+    try:
+        file = open(filepath,'r')
+        file.close()
+    except:
+        print("\nError. Either file doesn't exist or an error occured.\n")
+        return
+    else:
+        shutil.copyfile(filepath,copypath)
---
+++ interact/sqlops.py
@@ -0,0 +1,245 @@
+try:
+    import mysql.connector
+except:
+    print("mysql-connector-python (installation reqd. - yes)")
+    time.sleep(3)
+    quit()
+else:
+    pass
+
+syner = ("Syntax for updates : updates=[(<table1>,<targetcolumn1>,<newdata1 (int or string)>,"
+         "<conditioncolumn1 (can be blank by entering '')>,<pattern1> (can be blank by entering '')),\n"
+         "                             (<table2>,<targetcolumn2>,<newdata2 (int or string)>,"
+         "<conditioncolumn2 (can be blank by entering '')>,<pattern2> (can be blank by entering '')),\n"
+         "                          ...(<table'n'>,<targetcolumn'n'>,<newdata'n' (int or string)>,"
+         "<conditioncolumn'n' (can be blank by entering '')>,<pattern'n'> (can be blank by entering ''))]")
+
+
+def login(sqlhost="localhost",sqluser="",sqlpass="",sql_db="",dbtable="credentials"):
+    usern = ""
+    passw = ""
+    valid = 0
+    
+    while True:
+        usern = input("Username : ")
+        if usern.strip() == "":
+            print("\nUsername must be filled.\n")
+            continue
+        break
+    while True:
+        passw = getpass.getpass(prompt="Password : ")
+        if passw.strip() == "":
+            print("\nPassword must be filled.\n")
+            continue
+        break
+
+    if sql_db.strip()=="":
+        pass
+    else:
+        try:
+            mydb = mysql.connector.connect(
+                host = sqlhost.strip(),
+                user = sqluser.strip(),
+                passwd = sqlpass,
+                database = sql_db.strip()
+                )
+
+            cursor = mydb.cursor()
+        except:
+            print("\nError. Couldn't connect to database. Probably because database doesn't exist, invalid credentials or invalid database name.\n")
+            valid = 0
+        else:
+            cursor.execute(f"create table if not exists {dbtable.strip()} (m_id varchar(5) not null unique,Username varchar(20) not null unique , Password varchar(20) not null unique)")
+            cursor.execute(f"select * from {dbtable.strip()}")
+            for i in cursor:
+                mid,us,pa = i
+                if usern == us and passw == pa:
+                    print("\nCredentials valid.\n")
+                    valid = 1
+                    break
+            else:
+                print("\nInvalid credentials.\n")
+                valid = 0
+                
+    return(mid , usern , passw ,valid)
+
+
+
+def get_items(sqlhost="localhost",sqluser="",sqlpass="",sql_db="",dbtable="",reqcol="*",wherecol="",pattern=""):
+    itemlist = []
+    try:
+        mydb = mysql.connector.connect(
+            host = sqlhost.strip(),
+            user = sqluser.strip(),
+            passwd = sqlpass,
+            database = sql_db.strip()
+            )
+
+        cursor = mydb.cursor()
+    except:
+        print("\nError. Couldn't connect to database. Probably because database doesn't exist, invalid credentials or invalid database name.\n")
+        return
+    else:
+        if wherecol.strip()=="":
+            try:
+                cursor.execute(f"select {reqcol.strip()} from {dbtable.strip()}")
+            except:
+                print("\nError. Check your arguments again.\n")
+                return
+            else:
+                for item in cursor:
+                    itemlist.append(item)
+                return itemlist
+        else:
+            if pattern.strip()=="":
+                print("\nPattern required.\n")
+                return
+            else:
+                try:
+                    cursor.execute(f"select {reqcol.strip()} from {dbtable.strip()} where {wherecol.strip()} like '{pattern}'")
+                except:
+                    print("\nError. Check your arguments again.\n")
+                    return
+                else:
+                    for item in cursor:
+                        itemlist.append(item)
+                    return itemlist
+
+
+
+def update(sqlhost="localhost",sqluser="",sqlpass="",sql_db="",updates=[]):
+    global syner
+    if isinstance(updates , list) == False:
+        print("ArgTypeError : multi_update(<string> , <string> , <string> , <list>)")
+        return
+    try:
+        mydb = mysql.connector.connect(
+            host = sqlhost.strip(),
+            user = sqluser.strip(),
+            passwd = sqlpass,
+            database = sql_db.strip()
+            )
+
+        cursor = mydb.cursor()
+    except:
+        print("\nError. Couldn't connect to database. Probably because database doesn't exist, invalid credentials or invalid database name.\n")
+        return
+    else:
+        number = 1
+        for up in updates:
+            try:
+                if len(up)==3:
+                    dbtable,reqcol,newdata = up
+                elif len(up)==5:
+                    dbtable,reqcol,newdata,wherecol,pattern = up
+            except:
+                print(f"\nError in query {number}. Probably missing arguments or invalid data.\n")
+                print(syner)
+                number+=1
+                continue
+            else:
+                pass
+            
+            if str(newdata).strip() == "":
+                print(f"\nError in query {number}. New data can't be blank.\n")
+                print(syner)
+                number+=1
+                continue
+        
+            if len(up)==3:
+                try:
+                    cursor.execute(f"update {dbtable.strip()} set {reqcol.strip()} = {newdata}")
+                    mydb.commit()
+                except:
+                    print(f"\nError in query {number}. Check your arguments again.\n")
+                    print(syner)
+                    number+=1
+                    continue
+                else:
+                    number+=1
+                    continue
+            else:
+                if pattern.strip()=="":
+                    print(f"\nError in query {number}. Pattern required.\n")
+                    print(syner)
+                    number+=1
+                    continue
+                else:
+                    try:
+                        cursor.execute(f"update {dbtable.strip()} set {reqcol.strip()} = {newdata} where {wherecol.strip()} like '{pattern}'")
+                        mydb.commit()
+                    except:
+                        print(f"\nError in query {number}. Check your arguments again.\n")
+                        print(syner)
+                        number+=1
+                        continue
+                    else:
+                        number+=1
+                        continue
+                
+
+
+def sign_up(sqlhost="localhost",sqluser="",sqlpass="",sql_db="",dbtable="credentials"):
+    usern = ""
+    passw = ""
+    valid = 0
+
+    while True:
+        usern = input("Username : ")
+        if usern.strip() == "":
+            print("\nUsername must be filled.\n")
+            continue
+        break
+    while True:
+        passw = getpass.getpass(prompt="Password : ")
+        if passw.strip() == "":
+            print("\nPassword must be filled.\n")
+            continue
+        break
+    while True:
+        repassw = getpass.getpass(prompt="Confirm password : ")
+        if repassw.strip() == "":
+            print("\nPassword must be filled.\n")
+            continue
+        
+        if repassw != passw:
+            print("\nPasswords do not match.\n")
+            continue
+        elif repassw == passw:
+            break
+
+    if sql_db.strip()=="":
+        pass
+    else:
+        try:
+            mydb = mysql.connector.connect(
+                host = sqlhost.strip(),
+                user = sqluser.strip(),
+                passwd = sqlpass,
+                database = sql_db.strip()
+                )
+
+            cursor = mydb.cursor()
+        except:
+            print("\nError. Couldn't connect to database. Probably because database doesn't exist, invalid credentials or invalid database name.\n")
+            valid = 0
+        else:
+            try:
+                mid=""
+                no1=random.randint(0,9)
+                no2=random.randint(10,99)
+                ch1=random.randint(65,90)
+                ch2=random.randint(65,90)
+                mid=str(chr(ch1))+str(no1)+str(chr(ch2))+str(no2)
+                print(f"\nYour m_id : {mid}\n")
+                cursor.execute(f"create table if not exists {dbtable.strip()} (m_id varchar(5) not null unique,Username varchar(20) not null unique , Password varchar(20) not null unique)")
+                cursor.execute(f"insert into {dbtable.strip()} values('{mid}','{usern}','{passw}')")
+                mydb.commit()
+            except:
+                print("\nError. Couldn't register. Possibly because username/password/m_id already exists or invalid username/password (No special characters other than '_' allowed).\n")
+                valid = 0
+            else:
+                print("\nYou have been registered. Welcome.\n")
+                valid = 1
+                
+    return(mid , usern , passw ,valid)
