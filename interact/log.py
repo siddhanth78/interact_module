ea06d0d45134c00bdd0445e7ac5049244042fb74
Xstinkshun <61350672+siddhanth78@users.noreply.github.com>
2020-05-19 12:46:38 +0530

    Add files via upload

---
+++ interact/log.py
@@ -0,0 +1,112 @@
+try:
+    import mysql.connector
+except:
+    print("mysql-connector-python (installation reqd. - yes)")
+    time.sleep(3)
+    quit()
+else:
+    pass
+
+
+def savedata(sqlpass="",sqluser="root",sqlhost="localhost",key="",data=""):
+    try:
+        mydb = mysql.connector.connect(
+            host = sqlhost.strip(),
+            user = sqluser.strip(),
+            passwd = sqlpass
+            )
+        cursor = mydb.cursor()
+    except:
+        print("\nError. Couldn't connect to server. Probably because invalid credentials.\n")
+        return
+    else:
+        try:
+            cursor.execute("create database if not exists shortcuts")
+            cursor.execute("use shortcuts")
+            cursor.execute("create table if not exists shortcutlog(_key_ varchar(10) not null unique,_data_ varchar(300) not null unique)")
+            cursor.execute(f"insert into shortcutlog values('{key}','{data}')")
+            mydb.commit()
+        except:
+            print("\nInvalid data.\n")
+            return
+        else:
+            return
+    
+
+def getdata(sqlpass="",sqluser="root",sqlhost="localhost",key=""):
+    p=""
+    try:
+        mydb = mysql.connector.connect(
+            host = sqlhost.strip(),
+            user = sqluser.strip(),
+            passwd = sqlpass,
+            database = "shortcuts"
+            )
+        cursor = mydb.cursor()
+    except:
+        print("\nError. Couldn't connect to database. Probably because database doesn't exist, invalid credentials or invalid database name.\n")
+        return
+    else:
+        lik=[]
+        cursor.execute(f"select _key_ from shortcutlog")
+        for k in cursor:
+            lik.append(k)
+        if key not in lik:
+            print("\nKey not found.\n")
+            return p
+        else:
+            cursor.execute(f"select _data_ from shortcutlog where _key_ = '{key}'")
+            for x in cursor:
+                p = x[0]
+            return p
+
+
+def deletedata(sqlpass="",sqluser="root",sqlhost="localhost",key=""):
+    try:
+        mydb = mysql.connector.connect(
+            host = sqlhost.strip(),
+            user = sqluser.strip(),
+            passwd = sqlpass,
+            database = "shortcuts"
+            )
+        cursor = mydb.cursor()
+    except:
+        print("\nError. Couldn't connect to database. Probably because database doesn't exist, invalid credentials or invalid database name.\n")
+        return
+    else:
+        lik=[]
+        cursor.execute(f"select _key_ from shortcutlog")
+        for k in cursor:
+            lik.append(k)
+        if key not in lik:
+            print("\nKey not found.\n")
+            return
+        else:
+            cursor.execute(f"delete from shortcutlog where _key_ = '{key}'")
+            mydb.commit()
+            return
+
+def updatedata(sqlpass="",sqluser="root",sqlhost="localhost",key="",newdata=""):
+    try:
+        mydb = mysql.connector.connect(
+            host = sqlhost.strip(),
+            user = sqluser.strip(),
+            passwd = sqlpass,
+            database = "shortcuts"
+            )
+        cursor = mydb.cursor()
+    except:
+        print("\nError. Couldn't connect to database. Probably because database doesn't exist, invalid credentials or invalid database name.\n")
+        return
+    else:
+        lik=[]
+        cursor.execute(f"select _key_ from shortcutlog")
+        for k in cursor:
+            lik.append(k)
+        if key not in lik:
+            print("\nKey not found.\n")
+            return
+        else:
+            cursor.execute(f"update shortcutlog set _data_ = '{newdata}' where _key_ = '{key}'")
+            mydb.commit()
+            return
