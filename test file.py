a810c0f5870ff9ba8775fe0cee0706adb4fbd313
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2021-09-29 17:48:34 -0600

    test file v2

--- test file.py
+++ test file.py
@@ -1,3 +1,20 @@
-name = "Sid"
+import pandas as pd
+import numpy as np
+import plotly.express as px
 
-print(f"Hello {name}")
+xvals = np.arange(-10,10,0.01)
+
+df = pd.DataFrame({
+                    "x":xvals,
+                    "sin x":np.sin(xvals)
+                })
+                
+fig = px.line(df, x = "x", y = "sin x", template = "plotly_dark", title = "Sin x graph")
+
+fig.update_xaxes(title = "X")
+fig.update_yaxes(title = "Y")
+
+fig.update_layout(legend = dict(title = None, orientation = "h", yanchor = "bottom", y = 1, xanchor = "center", x=0.5),
+                    title = dict(yanchor = "top", y = 0.95, xanchor = "center", x=0.5))
+
+fig.show()
