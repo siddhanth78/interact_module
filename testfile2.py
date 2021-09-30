94c9c07492d9dbb0914767c5d027894d0e75347a
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2021-09-30 09:44:26 -0600

    Added new column to testfile2

--- testfile2.py
+++ testfile2.py
@@ -4,7 +4,8 @@ import numpy as np
 
 df = pd.DataFrame({
                     "column1":np.array([1,2,3]),
-                    "column2":np.array([4,5,6])
+                    "column2":np.array([4,5,6]),
+                    "column3":np.array([7,8,9])
 })
 
 print(df)
