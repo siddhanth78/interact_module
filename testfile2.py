0f60d8fab111d3cde7abd4c6d00a5ecd444aa1a7
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2021-09-30 09:42:18 -0600

    Added testfile2

---
+++ testfile2.py
@@ -0,0 +1,10 @@
+import random
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+                    "column1":np.array([1,2,3]),
+                    "column2":np.array([4,5,6])
+})
+
+print(df)
