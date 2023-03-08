bd336436941a0264723394981d1ea52f0d44ba2f
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2023-03-08 12:42:00 -0700

    Added GPA option

--- dataclassTest.py
+++ dataclassTest.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass, asdict
 from dataclass_csv import DataclassReader
 import plotly.express as px
 import pandas as pd
+from dash import Dash, html, dcc, Input, Output
+
+#Obviously, DataFrame.read_csv is the better option
 
 @dataclass
 class Student():
@@ -10,34 +13,67 @@ class Student():
     age: int
     grade: str = None
     score: int = None
+    gpa: float = None
 
-students = []
-with open("Students.csv") as users_csv:
-    reader = DataclassReader(users_csv, Student)
-    for row in reader:
-        students.append(row)
+app = Dash()
 
-keys = []
-names = []
-age = []
-scores = []
+app.layout = html.Div([
+                    html.H4("Scores"),
+                    html.P("Box graph of scores"),
+                    dcc.RadioItems(
+                                    id = 'info-options',
+                                    options = ['Scores', 'GPA'],
+                                    value = 'Scores'
+                                ),
+                    dcc.Dropdown(
+                                    id = 'color-options',
+                                    options = ['Inferno', 'Tealgrn', 'Oranges', 'Rainbow'],
+                                    value = 'Inferno'
+                                ),
+                    html.Div(id = 'output')
+                ])
 
-student_dict = {}
+@app.callback(
+                Output("output", "children"),
+                [Input("color-options", "value"), Input('info-options', "value")]
+            )
+def render_graph(color_option, info_option):
 
-for row in students:
-    keys.append(row.key)
-    names.append(row.name)
-    age.append(row.age)
-    scores.append(row.score)
+    students = []
+    with open("Students.csv") as users_csv:
+        reader = DataclassReader(users_csv, Student)
+        for row in reader:
+            students.append(row)
+
+    keys = []
+    names = []
+    age = []
+    scores = []
+    gpa = []
+
+    student_dict = {}
+
+    for row in students:
+        keys.append(row.key)
+        names.append(row.name)
+        age.append(row.age)
+        scores.append(row.score)
+        gpa.append(row.gpa)
     
-student_dict["keys"] = keys
-student_dict["names"] = names
-student_dict["age"] = age
-student_dict["scores"] = scores
+    student_dict["keys"] = keys
+    student_dict["names"] = names
+    student_dict["age"] = age
+    student_dict["scores"] = scores
+    student_dict["gpa"] = gpa
     
-df = pd.DataFrame(student_dict)
+    df = pd.DataFrame(student_dict)
+    if info_option == "Scores":
+        fig = px.bar(df, x = "names", y = "scores", color = 'scores', color_continuous_scale = color_option, title = f"Graph with color {color_option}")
+    elif info_option == "GPA":
+        fig = px.bar(df, x = "names", y = "gpa", color = 'gpa', color_continuous_scale = color_option, title = f"Graph with color {color_option}")
+    return dcc.Graph(figure = fig)
+    
 
-fig = px.bar(df, x = "names", y = "scores", template = "plotly_dark", color = "scores")
-fig.show()
+app.run_server(debug = True)
 
 
