58429a03f34b45703c061aaf027d2bec4ac8e973
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2023-07-08 10:01:00 -0600

    fixed gmail mark read

--- GmailMarkAllRead.py
+++ GmailMarkAllRead.py
@@ -1,43 +1,35 @@
-import pyautogui
+import pyautogui as pag
 import time
 
-openx = 772
-openy = 1056
 
-closex = 1902
-closey = 17
+minimize = pag.locateOnScreen("min.png", confidence = 0.9, grayscale=True)
 
-select_all_x = 148
-select_all_y = 237
+pag.moveTo(minimize)
+pag.click()
 
-markreadx = 463
-markready = 243
-
-linkx = 259
-linky = 436
-
-
-pyautogui.click(openx, openy)
-time.sleep(0.5)
-pyautogui.hotkey('ctrl', 't')
-time.sleep(0.5)
-pyautogui.typewrite('gmail')
-time.sleep(0.5)
-pyautogui.press('enter')
 time.sleep(1)
-pyautogui.click(linkx, linky)
+
+open_chrome = pag.locateOnScreen("chrome_icon.png", confidence = 0.8, grayscale=True)
+
+pag.moveTo(open_chrome)
+pag.click(clicks = 2)
+    
+time.sleep(3)
+
+pag.typewrite('gmail.com')
+pag.press('enter')
 time.sleep(2)
-pyautogui.click(select_all_x, select_all_y)
-time.sleep(1)
-pyautogui.click(markreadx, markready)
-time.sleep(1)
-pyautogui.click(select_all_x, select_all_y)
-time.sleep(1)
-pyautogui.click(closex, closey)
 
-'''
-while True:
-    print(pyautogui.position())
-    time.sleep(0.5)
+select_all = pag.locateOnScreen("checkbox.png", confidence=0.9, grayscale=True)
 
-'''
+pag.moveTo(select_all)
+pag.click()
+
+mark_as_read = pag.locateOnScreen("mark_as_read.png", confidence=0.9, grayscale=True)
+
+pag.moveTo(mark_as_read)
+pag.click()
+
+time.sleep(0.5)
+
+pag.hotkey('ctrl', 'w')
